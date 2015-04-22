2d1e9f0186a2f38512fc70722712725e262fb032
rchwlsk2 <rchwlsk2@illinois.edu>
2015-04-22 13:56:08 -0500

    Basic client works now

--- client/__init__.py
+++ client/__init__.py
@@ -3,5 +3,6 @@ from client.downloader import *
 from client.file_manager import *
 from client.network_connection import *
 from client.uploader import *
+from client.pTorrentClient import pTorrentClient
 
 __author__ = 'paulrachwalski'
